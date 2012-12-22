0abd5e118a3ddad07f57569fa890e74912f5647e
Matthew Frost <mf@matthewfrost.com>
2012-12-22 16:12:09 -0500

    problem 1 complete

--- Problem1/src/Problem1.cpp
+++ Problem1/src/Problem1.cpp
@@ -7,6 +7,12 @@
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-	std::cout << "" << std::endl;
+	int sum = 0;
+	for (int i=0; i<1000; i++) {
+		if (i%3==0 || i%5==0) {
+			sum += i;
+		}
+	}
+	std::cout << sum << std::endl;
 	return 0;
 }
