03101ccc131c11dcd64f4a60c20ce05c700979d0
Matthew Frost <mf@matthewfrost.com>
2012-12-22 16:37:40 -0500

    sum calculated

--- Problem2/src/Problem2.cpp
+++ Problem2/src/Problem2.cpp
@@ -6,24 +6,27 @@
 
 #include <iostream>
 
-int fib(int a, int b, int max);
+int fib(int a, int b, int sum, int max);
 
 int main(int argc, char* argv[]) {
 	//const int MAX = 4000000;
-	const int MAX = 100;
-	int sum = fib(1, 2, MAX);
+	const int MAX = 1000;
+	int sum = fib(1, 2, 0, MAX);
     std::cout << sum << std::endl;
     return 0;
 }
 
-int fib(int a, int b, int max) {
+int fib(int a, int b, int sum, int max) {
 	std::cout << a << "\t" << b << std::endl;
 	if (a>max) {
-		return a;
+		return sum;
+	}
+	if (a%2==0) {
+		sum += a;
 	}
 	int temp = a+b;
 	a = b;
 	b = temp;
-	return fib(a, b, max);
+	return fib(a, b, sum, max);
 }
 
