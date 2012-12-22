3dffa2b1aa640114b519b1ee5f8aea846f08b8c0
Matthew Frost <mf@matthewfrost.com>
2012-12-22 16:08:08 -0500

    created main template

---
+++ Problem1/src/Problem1.cpp
@@ -0,0 +1,12 @@
+//============================================================================
+// Name        : Problem1.cpp
+// Author      : Matthew Frost
+// Description : Find the sum of all the multiples of 3 or 5 below 1000
+//============================================================================
+
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+	std::cout << "" << std::endl;
+	return 0;
+}
