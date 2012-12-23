7824a8291cd21457e1e34080fe3dc72bcec40d0d
Matthew Frost <mf@matthewfrost.com>
2012-12-23 14:05:39 -0500

    counts digits in product

--- Problem4/src/Problem4.cpp
+++ Problem4/src/Problem4.cpp
@@ -7,11 +7,12 @@
 #include <iostream>
 
 bool isPalindrome(int num);
+int countDigits(int num);
 
 int main(int argc, char* argv[]) {
 	int highest = 0;
-	for (int i=100; i<1000; i++) {
-		for (int j=100; j<1000; j++) {
+	for (int i=1; i<105; i++) {
+		for (int j=10; j<105; j++) {
 			int product = i*j;
 			if (isPalindrome(product) && product>highest) {
 				highest = product;
@@ -23,6 +24,20 @@ int main(int argc, char* argv[]) {
 }
 
 bool isPalindrome(int num) {
+	if (num < 10) {
+		return num >= 0;
+	}
+	int digits = countDigits(num);
+	std::cout << num << "\t" << digits << std::endl;
 	return true;
 }
 
+int countDigits(int num) {
+	int digits = 0;
+	while (num > 0) {
+		num /= 10;
+		digits++;
+	}
+
+	return digits;
+}
