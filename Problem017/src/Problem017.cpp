c717747ab99b879478f2d63db58d45595b130614
Matthew Frost <mf@matthewfrost.com>
2013-02-06 19:53:06 -0500

    still wrong :-(

--- Problem017/src/Problem017.cpp
+++ Problem017/src/Problem017.cpp
@@ -11,8 +11,8 @@ int getLength(std::string* strings, int size);
 
 int main(int argc, char* argv[]) {
 	std::string ones[] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-	std::string teens[] = {"eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
-	std::string tens[] = {"ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+	std::string teens[] = {"ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+	std::string tens[] = {"twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
 	const int onesLength = getLength(ones, sizeof(ones) / sizeof(std::string));
 	const int teensLength = getLength(teens, sizeof(teens) / sizeof(std::string));
 	const int tensLength = getLength(tens, sizeof(tens) / sizeof(std::string));
@@ -22,27 +22,27 @@ int main(int argc, char* argv[]) {
 	int total = 0;
 
 	//one thousand
-	total += ((new std::string("one thousand"))->length()-1);
+	total += ((std::string("one thousand")).length()-1);
 	//hundreds in 1000 - handle "hundred" in "one hundred"
 	int numHundreds = 1000 - 100;
 	int combinedHundredLengths = numHundreds * hundredLength;
-	//hundreds in 1000 - handle "xyz" in "xyz hundred"
-	combinedHundredLengths += onesLength * 100;
 	total += combinedHundredLengths;
+	//hundreds in 1000 - handle "xyz" in "xyz hundred"
+	total += onesLength * 100;
 	//tens in 1000
-	int numTens = (1000 / 100) * 9;
+	int numTens = 10 * 10;
 	int combinedTensLengths = numTens * tensLength;
 	total += combinedTensLengths;
 	//ones in 1000
-	int numOnes = (1000 / 100) * 9; //9 instead of 10 because not used by teens
+	int numOnes = 10 * 9; //9 instead of 10 because not used by teens
 	int combinedOnesLengths = numOnes * onesLength;
 	total += combinedOnesLengths;
 	//teens in 1000
-	int numTeens = 1000 / 100;
+	int numTeens = 10;
 	int combinedTeensLengths = numTeens * teensLength;
 	total += combinedTeensLengths;
 	//ands in 1000
-	int numAnds = numHundreds - (1000 / 100);
+	int numAnds = numHundreds - 10; //only x00 won't have "and"
 	int combinedAndLengths = numAnds * andLength;
 	total += combinedAndLengths;
 
