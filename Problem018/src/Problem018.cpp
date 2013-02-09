035a38486ea68feb6d237d0bd8c14d122643b5b4
Matthew Frost <mf@matthewfrost.com>
2013-02-09 14:52:34 -0500

    triangle populated

--- Problem018/src/Problem018.cpp
+++ Problem018/src/Problem018.cpp
@@ -7,12 +7,15 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <string.h>
+#include <stdlib.h>
 
 std::vector<std::vector<int> > createTriangle();
+std::vector<int> tokenize(std::string str);
 
 int main(int argc, char* argv[]) {
 	std::vector<std::vector<int> > triangle = createTriangle();
-
+	std::cout << triangle.size() << std::endl;	//DELME
 	int max = 0;
     std::cout << "The maximum sum of the triangle is " << max << std::endl;
     return 0;
@@ -25,10 +28,25 @@ std::vector<std::vector<int> > createTriangle() {
 		while (file.good()) {
 			std::string line;
 			getline(file, line);
-			std::cout << line << std::endl;
+			std::cout << line << std::endl;	//DELME
+			triangle.push_back(tokenize(line));
 		}
 		file.close();
 	}
 	return triangle;
+}
+
+std::vector<int> tokenize(std::string str) {
+	std::vector<int> v;
+	for (unsigned int i=0; i<=str.length(); i++) {
+		if (i==str.length() || !std::isdigit(str[i])) {
+			int num = ((str[i-2]-48) * 10) + (str[i-1]-48);
+			std::cout << num;	//DELME
+			v.push_back(num);
+		}
+	}
+	std::cout << std::endl;	//DELME
+	return v;
+
 }
 
