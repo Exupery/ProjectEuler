7081953fdf3d374a11b3b48898e4c984be992f5c
Matthew Frost <mf@matthewfrost.com>
2013-04-06 11:18:14 -0400

    started problem 19

---
+++ Problem019/src/Problem019.cpp
@@ -0,0 +1,14 @@
+//============================================================================
+// Name        : ProblemX.cpp
+// Author      : Matthew Frost
+// Description : How many Sundays fell on the first of the month during the
+//				 twentieth century (1 Jan 1901 to 31 Dec 2000)
+//============================================================================
+
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    std::cout << "" << std::endl;
+    return 0;
+}
+
