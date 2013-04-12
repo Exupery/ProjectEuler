957b6178658ad18f4fa165fe2d8c6b325d2b8728
Matthew Frost <mf@matthewfrost.com>
2013-04-12 16:06:00 -0400

    added number input

--- Problem020/src/Problem020.cpp
+++ Problem020/src/Problem020.cpp
@@ -6,10 +6,19 @@
 
 #include <iostream>
 
+int calcFactorial(int num);
+
 int main(int argc, char* argv[]) {
 	int num = 0;
+	std::cout << "Enter a number: ";
+	std::cin >> num;
+	int factorial = calcFactorial(num);
 	int sum = 0;
-    std::cout << "The sum of the digits of " << num << "! is " << sum << std::endl;
+    std::cout << "The sum of the digits of " << num << "! (" << factorial << ") is " << sum << std::endl;
     return 0;
+}
+
+int calcFactorial(int num) {
+	return num;
 }
 
