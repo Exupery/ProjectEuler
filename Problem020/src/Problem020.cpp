1b61319e3138fc1a9c221364b941f548cdd722f8
Matthew Frost <mf@matthewfrost.com>
2013-04-12 16:23:28 -0400

    factorial calculated

--- Problem020/src/Problem020.cpp
+++ Problem020/src/Problem020.cpp
@@ -6,19 +6,24 @@
 
 #include <iostream>
 
-int calcFactorial(int num);
+int calcFactorial(int num, int sum);
 
 int main(int argc, char* argv[]) {
 	int num = 0;
 	std::cout << "Enter a number: ";
 	std::cin >> num;
-	int factorial = calcFactorial(num);
+	int factorial = calcFactorial(num, 0);
 	int sum = 0;
     std::cout << "The sum of the digits of " << num << "! (" << factorial << ") is " << sum << std::endl;
     return 0;
 }
 
-int calcFactorial(int num) {
-	return num;
+int calcFactorial(int num, int fct) {
+	if (num == 0) {
+		return fct;
+	} else {
+		fct = (fct==0) ? num : fct * num;
+		return calcFactorial(num-1, fct);
+	}
 }
 
