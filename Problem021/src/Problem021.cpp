edb3a7b5cfc521b628a37629dba58cf3ceab9ef6
Matthew Frost <mf@matthewfrost.com>
2013-05-12 11:12:22 -0400

    started problem 21

---
+++ Problem021/src/Problem021.cpp
@@ -0,0 +1,13 @@
+//============================================================================
+// Name        : Problem021.cpp
+// Author      : Matthew Frost
+// Description : Evaluate the sum of all the amicable numbers under 10000
+//============================================================================
+
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    std::cout << "" << std::endl;
+    return 0;
+}
+
