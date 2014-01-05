56b8c53d51f0edc55ad9e09a53d9d31569479fc9
Matthew Frost <mf@matthewfrost.com>
2014-01-05 13:13:29 -0500

    build up from digits

--- Problem024/src/Problem024.cpp
+++ Problem024/src/Problem024.cpp
@@ -5,54 +5,37 @@
 //============================================================================
 
 #include <iostream>
-#include <unordered_map>
 #include <set>
 #include <vector>
+#include <algorithm>
 
-std::set<std::vector<int>> fill(int pos, std::vector<int> digits);
+std::set<std::vector<int>> fill(std::vector<int> digits);
 
 int main(int argc, char* argv[]) {
-	std::vector<int> digits = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-	std::set<std::vector<int>> permutations;
-	for (int d : digits) {
-		std::set<std::vector<int>> filled = fill(d, digits);
-		permutations.insert(filled.begin(), filled.end());
-	}
+//	std::vector<int> digits {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	std::vector<int> digits {0, 1, 2};
+	std::set<std::vector<int>> permutations = fill(digits);
     std::cout << "Found " << permutations.size() << " permutations" << std::endl;
     return 0;
 }
 
-std::set<std::vector<int>> fill(int pos, std::vector<int> digits) {
+std::set<std::vector<int>> fill(std::vector<int> digits) {
 	std::set<std::vector<int>> filled;
-	std::unordered_map<int, int> valuesPositions;
-	std::unordered_map<int, int> positionsValues;
-	for (int d : digits) {
-		auto initPair = std::pair<int, int>(d, d);
-		valuesPositions.insert(initPair);
-		positionsValues.insert(initPair);
+	std::set<std::vector<int>> perms;
+	for (auto d : digits) {
+		std::vector<int> initial(d);
+		perms.insert(initial);
 	}
-	for (unsigned int i = 0; i < digits.size(); i++) {
-		int valAtPos = positionsValues.find(pos)->second;
-		int prevPos = valuesPositions.find(i)->second;
-		positionsValues.erase(pos);
-		positionsValues.erase(prevPos);
-		valuesPositions.erase(i);
-		valuesPositions.erase(valAtPos);
-		positionsValues.insert(std::pair<int, int>(pos, i));
-		positionsValues.insert(std::pair<int, int>(prevPos, valAtPos));
-		valuesPositions.insert(std::pair<int, int>(i, pos));
-		valuesPositions.insert(std::pair<int, int>(valAtPos, prevPos));
-		std::vector<int> filledPerm;
-		for (auto p : positionsValues) {
-			filledPerm.push_back(p.second);
+	for (auto perm : perms) {
+		while (perm.size() != digits.size()) {
+			for (auto d : digits) {
+				if (std::find(std::begin(perm), std::end(perm), d) == perm.end()) {
+					perm.push_back(d);
+					std::cout << perm.size() << "\t" << d << std::endl;	//DELME
+				}
+			}
 		}
-		filled.insert(filledPerm);
-		std::vector<int> reversed;
-		for (int j = filledPerm.size()-1; j >= 0; j--) {
-			reversed.push_back(filledPerm.at(j));
-		}
-		filled.insert(reversed);
+		filled.insert(perm);
 	}
 	return filled;
 }
-
