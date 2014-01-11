ade51ff5deea7e26e43c2816f76ec70d4e0d9692
Matthew Frost <mf@matthewfrost.com>
2014-01-11 13:57:53 -0500

    recrusively swap elements

--- Problem024/src/Problem024.cpp
+++ Problem024/src/Problem024.cpp
@@ -9,33 +9,32 @@
 #include <vector>
 #include <algorithm>
 
-std::set<std::vector<int>> fill(std::vector<int> digits);
+std::set<std::vector<int>> fill(std::vector<int> digits, std::set<std::vector<int>> perms);
 
 int main(int argc, char* argv[]) {
 //	std::vector<int> digits {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 	std::vector<int> digits {0, 1, 2};
-	std::set<std::vector<int>> permutations = fill(digits);
+	std::set<std::vector<int>> initial {digits};
+	std::set<std::vector<int>> permutations = fill(digits, initial);
     std::cout << "Found " << permutations.size() << " permutations" << std::endl;
     return 0;
 }
 
-std::set<std::vector<int>> fill(std::vector<int> digits) {
-	std::set<std::vector<int>> filled;
-	std::set<std::vector<int>> perms;
-	for (auto d : digits) {
-		std::vector<int> initial(d);
-		perms.insert(initial);
-	}
+std::set<std::vector<int>> fill(std::vector<int> digits, std::set<std::vector<int>> perms) {
+	std::set<std::vector<int>> filled(perms);
 	for (auto perm : perms) {
-		while (perm.size() != digits.size()) {
-			for (auto d : digits) {
-				if (std::find(std::begin(perm), std::end(perm), d) == perm.end()) {
-					perm.push_back(d);
-					std::cout << perm.size() << "\t" << d << std::endl;	//DELME
-				}
+		for (auto digit : digits) {
+			std::vector<int> p(perm);
+			auto pos = std::find(std::begin(p), std::end(p), digit);
+			if (pos == p.begin()) {
+				//swap first and last elements
+				std::cout << p.size() << "\t" << digit << std::endl;	//DELME
+			} else {
+				//swap pos with pos-1
 			}
+			filled.insert(p);
+			//reverse
 		}
-		filled.insert(perm);
 	}
-	return filled;
+	return (filled.size() == perms.size()) ? filled : fill(digits, filled);
 }
